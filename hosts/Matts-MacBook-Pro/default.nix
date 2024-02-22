defe2ef28334140b64868eb12f08e8deadde13c7
Matt Wynne <matt.wynne@mechanical-orchard.com>
2024-02-21 16:48:33 -0800

    Initial commit

---
+++ hosts/Matts-MacBook-Pro/default.nix
@@ -0,0 +1,106 @@
+{pkgs, lib, ...}:
+
+{  
+	nixpkgs = {
+		config.allowUnfree = true;
+	};
+
+	nix.extraOptions = ''
+		auto-optimise-store = true
+		experimental-features = nix-command flakes
+		'' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+		extra-platforms = x86_64-darwin aarch64-darwin
+		'';
+
+	services.nix-daemon.enable = true;
+
+	programs.zsh.enable = true;
+
+	security.pam.enableSudoTouchIdAuth = true;
+
+	homebrew = {
+		enable = true;
+		onActivation = {
+			autoUpdate = true;
+			upgrade = true;
+		};
+
+		brews = [
+		];
+
+		casks = [
+#      "cursor"
+#      "logseq"
+#      "raycast"
+#      "slack"
+#      "tuple"
+#      "tandem"
+#      "warp"
+#      "arc"
+#      "readdle-spark"
+#      "docker"
+		];
+	}; 
+
+	system.defaults = {
+		dock.autohide = true;
+	};
+
+	home-manager = {
+		useGlobalPkgs = true;
+		useUserPackages = true;
+		users.matt = {pkgs, config, ...}: {
+			home = {
+				stateVersion = "23.11";
+				username = lib.mkDefault "matt";
+				homeDirectory = lib.mkForce "/Users/matt";
+
+				packages = with pkgs; [
+					_1password
+          docker
+          oh-my-zsh
+				];
+
+				sessionVariables = {
+					EDITOR = "vim";
+					SSH_AUTH_SOCK = "${config.home.homeDirectory}/.1password/agent.sock";
+				};
+
+				file.".1password/agent.sock" = lib.mkIf pkgs.stdenv.isDarwin {
+					source = config.lib.file.mkOutOfStoreSymlink
+						"${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+				};
+			};
+
+		programs.ssh = {
+			enable = true;
+			matchBlocks."*" = {
+				extraOptions = {
+					IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+				};
+			};
+		};
+
+		programs.home-manager.enable = true;
+		programs.direnv.enable = true;
+		programs.direnv.nix-direnv.enable = true;
+		programs.zsh.enable = true;
+
+		programs.vim = {
+			enable = true;
+			settings = {
+				number = true;
+				tabstop = 2;
+				expandtab = true;
+				shiftwidth = 2;
+			};
+		};
+	};
+
+	# TODO: git (see https://github.com/zgagnon/conf-flake/blob/master/hosts/Zells-MacBook-Pro/default.nix#L148)
+	};
+
+	nix.settings = {
+		auto-optimise-store = true;
+	};
+}
